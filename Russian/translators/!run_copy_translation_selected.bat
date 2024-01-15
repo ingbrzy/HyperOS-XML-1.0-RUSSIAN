7836af56614ba3e8adcfae2e6ccaaf34218d3cbd
mclaude74 <45774262+mclaude74@users.noreply.github.com>
2024-01-15 16:58:08 +0300

    add translation from Health connect (Google)

--- Russian/translators/!run_copy_translation_selected.bat
+++ Russian/translators/!run_copy_translation_selected.bat
@@ -5,6 +5,7 @@ if "%~1"=="" (
   goto _end_batch
 )
 ;
+call run_copy_translation_sel.bat %1 HealthConnectController
 call run_copy_translation_sel.bat %1 MiBrowserGlobal
 call run_copy_translation_sel.bat %1 MiuiVideoGlobal
 call run_copy_translation_sel.bat %1 Music
